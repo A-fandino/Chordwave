9ae582c48e4d01073c0231809c85b1506141a84e
A-fandino <arnauf8@gmail.com>
2022-04-19 17:49:42 +0200

    Frontend setup

---
+++ client/src/Layout/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <header className="p-4 bg-indigo-500 text-white font-bold text-2xl">
+        Chordwave
+    </header>
+  )
+}
---
+++ client/src/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Header from './Layout/Header/index'
+
+export default function App() {
+  return (
+    <Header/>
+  )
+}
