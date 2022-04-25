175fbf95def96fef9eed43395ae2fb7d651b1e20
A-fandino <arnauf8@gmail.com>
2022-04-25 16:40:16 +0200

    Access to file api and socketio

--- client/src/Pages/Upload/index.tsx
+++ client/src/Pages/Upload/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { ChangeEventHandler, JSXElementConstructor } from 'react'
 
 export default function Upload() {
+
+    const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const form = new FormData()
+        if (!event.target.files) return
+        form.append("file",event.target.files[0])
+
+        const resp = await fetch('http://localhost:5000/api/upload', {
+            method:"POST",
+            mode: "cors"
+        })
+        const data = await resp.text()
+        console.log(data)
+
+    }
+
     return ( //Temporal FORM
     <>
-    <form>
-        <input type="file" name="" id="" />
-    </form>
+        <input type="file" name="" id=""  onChange={handleUpload}/>
     </>
     )
     }
--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Routes,
@@ -7,8 +7,21 @@ import {
   } from "react-router-dom";
 import Home from './Pages/Home/index'
 import Upload from './Pages/Upload/index'
+import socketIOClient from "socket.io-client"
+const ENDPOINT = "http://localhost:5000"
 
 export default function App() {
+
+  useEffect((): any=> {
+    const socket = socketIOClient(ENDPOINT)
+    socket.on("test", data => {
+      socket.emit("ping")
+      console.log(data)
+    })
+
+    return () => socket.disconnect()
+  }, [])
+
   return (
       <Router>
           <Routes>
