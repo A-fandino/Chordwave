22805a67963a407712f38b660b1331d5610b0038
A-fandino <arnauf8@gmail.com>
2022-05-09 19:36:40 +0200

    configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext"
+  },
   plugins: [react()],
   server: {
         port: 80,
