19eb20b4dedeb03f7ac1f1a02e0c87dbef79472e
A-fandino <arnauf8@gmail.com>
2022-05-15 11:03:26 +0200

    Home scroll button

--- client/src/Layout/Header/index.jsx
+++ client/src/Layout/Header/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ChevronDoubleDownIcon } from '@heroicons/react/solid'
 import FancyText from "@/Components/FancyText"
 export default function Header() {
     return (
         <>
-    <header className="p-4 bg-gray-900 text-white font-bold text-2xl h-screen flex flex-col xl:flex-row shadow-xl">
+    <header className="p-4 bg-gray-900 text-white font-bold text-2xl h-screen relative flex flex-col xl:flex-row shadow-xl">
         <div className="flex flex-col justify-center items-center h-full">
             <FancyText size="big" classes="px-16 py-4"></FancyText>
-            <p className="text-sm text-gray-200 italic">Música por y para todos</p>
+            <p className="text-md text-gray-400 italic">Share your wave</p>
         </div>
         <div className='flex w-full items-center justify-center'>
             <Link to='/upload'><button className='bg-indigo-500 p-8 text-2xl font-bold rounded hover:bg-indigo-700'>Upload song</button></Link>
         </div>
+        <span className='xl:absolute static my-8 left-0 right-0 w-full bottom-16 flex justify-center h-20' onClick={() => scrollTo(0, screen.height)}>
+            <ChevronDoubleDownIcon className='h-full animate-bounce hover:bg-black/[.25] rounded-full p-4'/>
+        </span>
     </header>
     </>
     )
--- client/src/Pages/Liked/index.jsx
+++ client/src/Pages/Liked/index.jsx
@@ -19,11 +19,15 @@ export default function Liked() {
     <main className="flex flex-col gap-4 w-full">
         <Nav/>
         <h1 className="text-5xl p-8 pb-0 text-white font-bold">Your Likes</h1>
+        { songs.length ?
         <section className="flex flex-wrap gap-4 p-8">
             {
                 songs.map(song => <SongMiniature data={song}/>)
             }
         </section>
+        :
+        <span className='text-5xl text-white text-gray-300 text-center w-full font-bold italic h-full flex justify-center items-center h-96'>No liked songs</span>
+        }
     </main>
     )
 }
