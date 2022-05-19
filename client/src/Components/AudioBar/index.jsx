2b91256bd5b3d79ff7200b8e9614682efba462ba
A-fandino <arnauf8@gmail.com>
2022-05-19 19:50:39 +0200

    Fix next song not working when song ended

--- client/src/Components/AudioBar/index.jsx
+++ client/src/Components/AudioBar/index.jsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom';
 export default function AudioBar(props) {
     const padStart = (str) => (""+str).padStart(2,'0')
-    const navigate = useNavigate()
     let timer = null;
     const [percent, setPercent] = useState(0)
     const [current, setCurrent] = useState(0)
@@ -15,16 +13,14 @@ export default function AudioBar(props) {
         }
 
       const advance = function(duration) {
-        console.log(duration)
         const curr = audio.current?.currentTime ?? 0
         const increment = 10/duration
         let per = Math.min(increment * curr * 10, 100);
-        // console.table({curr,increment,per,duration})
         setPercent(per)
         setCurrent(curr)
-        if (!per || per < 100) return startTimer(duration)
-        setPlay(false)
+        if (!per || Math.ceil(per) < 100) return startTimer(duration)
         props.onFinish ? props.onFinish() : "";
+        setPlay(false)
       }
     useEffect(() => { 
             if (!play)clearTimeout(timer);
