569d9df2f2226eddf91e94e94aaa64a101058a2b
A-fandino <arnauf8@gmail.com>
2022-05-21 10:56:04 +0200

    Volume localstorage

--- client/src/Components/VolumeController/index.jsx
+++ client/src/Components/VolumeController/index.jsx
@@ -2,16 +2,24 @@ import React, {useEffect, useRef, useState} from 'react'
 import { VolumeUpIcon, VolumeOffIcon } from "@heroicons/react/solid"
 
 export default function VolumeController(props) {
-  const [volume, setVolume] = useState(props.audio?.volume || .5)
+  const [volume, setVolume] = useState(localStorage.getItem("volume") || props.audio?.volume || .5)
   const volumeBeforeMute = useRef(volume)
   useEffect(() => {
       if (props.audio) props.audio.volume = volume
+      localStorage.setItem("volume", volume)
+
   },[volume])
+
+
+  const handleChange = e => {
+    setVolume(e.target.value)
+  }
+
   return (
-    <div className="volumne-controller flex gap-4 w-max items-center font-bold">
+    <div className="volume-controller flex gap-4 w-max items-center font-bold">
         {volume > 0 ? <VolumeUpIcon onClick={() => {volumeBeforeMute.current=volume;setVolume(0)}} className='w-10 hover:text-gray-400'/> : <VolumeOffIcon onClick={() => setVolume(volumeBeforeMute.current || .1)} className='w-10 hover:text-gray-400'/>}
 
-        <input type="range" min="0" max="1" step="0.01" value={volume} onChange={e => setVolume(e.target.value)}></input>
+        <input type="range" min="0" max="1" step="0.01" value={volume} onChange={handleChange}></input>
 
         <span>{Math.floor(volume*100)}%</span>
     </div>
